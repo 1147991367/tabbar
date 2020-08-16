2339579cdd67668c10c7b0f7061bc89d903a26c0
liyingjing <2391429339@qq.com>
2020-08-16 14:11:41 +0800

    app拆分

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tabbar>
-      <tabbaritem path='/home'>
-         <i slot='icon-font' class="icon iconfont icon-shouye2"></i>
-        <div slot='icon-text'>首页</div>
-      </tabbaritem>
-      <tabbaritem path='/about'>
-        <i slot='icon-font' class="icon iconfont icon-icon-test"></i>
-          <div slot='icon-text'>分类</div>
-      </tabbaritem>
-      <tabbaritem path='/list'>
-         <i slot='icon-font' class="icon iconfont icon-dingdan3"></i>
-          <div slot='icon-text'>订单</div>
-      </tabbaritem>
-      <tabbaritem path='/profile'>
-          <i slot='icon-font' class="icon iconfont icon-gerenzhongxin3"></i>
-          <div slot='icon-text'>我的</div>
-      </tabbaritem>
-   </tabbar>
+    <mainTabbar></mainTabbar>
   </div>
 </template>
 <script>
-import tabbar from '@/components/tabbar/tabbar'
-import tabbaritem from '@/components/tabbar/tabbaritem'
+import mainTabbar from '@/components/tabbar/mainTabbar'
+
 export default {
     name:'App',
     components:{
-      tabbar,
-      tabbaritem,
+     mainTabbar
     }
 }
 </script>
---
+++ src/components/tabbar/mainTabbar.vue
@@ -0,0 +1,34 @@
+<template>
+ <tabbar>
+      <tabbaritem path='/home'>
+         <i slot='icon-font' class="icon iconfont icon-shouye2"></i>
+        <div slot='icon-text'>首页</div>
+      </tabbaritem>
+      <tabbaritem path='/about'>
+        <i slot='icon-font' class="icon iconfont icon-icon-test"></i>
+          <div slot='icon-text'>分类</div>
+      </tabbaritem>
+      <tabbaritem path='/list'>
+         <i slot='icon-font' class="icon iconfont icon-dingdan3"></i>
+          <div slot='icon-text'>订单</div>
+      </tabbaritem>
+      <tabbaritem path='/profile'>
+          <i slot='icon-font' class="icon iconfont icon-gerenzhongxin3"></i>
+          <div slot='icon-text'>我的</div>
+      </tabbaritem>
+   </tabbar>
+</template>
+
+<script>
+import tabbar from '@/components/tabbar/tabbar'
+import tabbaritem from '@/components/tabbar/tabbaritem'
+export default {
+    components:{
+         tabbar,
+      tabbaritem,
+    }
+}
+</script>
+<style lang="stylus" scoped>
+ 
+</style>
